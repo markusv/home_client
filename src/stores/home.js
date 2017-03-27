cfe71b8e5eab8f36752d75c9943038c5419851bd
Markus Voss <markus.voss@ehelse.no>
2017-03-27 20:18:54 +0200

    Fixed lint errors

--- src/stores/home.js
+++ src/stores/home.js
@@ -3,16 +3,16 @@ import thunk from 'redux-thunk';
 
 const rootReducer = (state, action) => {
   switch (action.type) {
-  case 'CHANGE_NAME':
-    return {
-      name: action.name
-    };
-  default:
-    return {
-      name: 'TOM'
-    };
+    case 'CHANGE_NAME':
+      return {
+        name: action.name
+      };
+    default:
+      return {
+        name: 'TOM'
+      };
   }
-}
+};
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 export default store;
