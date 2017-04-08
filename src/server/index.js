5659792ee9473b94af44e107171bede334046159
Markus Voss <markus.voss@ehelse.no>
2017-04-08 14:33:38 +0200

    Added favicon

--- src/server/index.js
+++ src/server/index.js
@@ -4,10 +4,12 @@ import serve from 'koa-static-server';
 import convert from 'koa-convert';
 import config from '../config/config';
 import proxy from 'koa-proxies';
+import favicon from 'koa-favicon';
 
 const app = new Koa();
 app.use(convert(serve({rootDir: path.join(__dirname, '..', '..', 'build')})));
 app.use(convert(serve({rootDir: path.join(__dirname, '..', '..', 'assets')})));
+app.use(favicon(path.join(__dirname, '..', '..', 'assets', 'favicon.ico')));
 
 // proxy all api calls to api server
 app.use(proxy('/api', {
