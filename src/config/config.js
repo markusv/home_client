1fcc6df100865dfb9bce88a9641dfbfd507078e9
Markus Voss <markus.voss@ehelse.no>
2017-04-17 10:37:51 +0200

    New weboscket proxy server

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export default {
   serverUrl: 'http://127.0.0.1:9333',
   wsServerUrl: 'http://127.0.0.1:8080',
-  clientWsUrl: process.env.NODE_ENV === 'production' ? 'ws://192.168.10.164:3000/': 'ws://localhost:3000/ws' //eslint-disable-line no-process-env
+  clientWsUrl: process.env.NODE_ENV === 'production' ? 'ws://192.168.10.164:8014': 'ws://localhost:3000/ws' //eslint-disable-line no-process-env
 };
--- src/server/index.js
+++ src/server/index.js
@@ -18,16 +18,10 @@ app.use(proxy('/api', {
   rewrite: (path) => path.replace(/^\/api/, '')
 }));
 
-// proxy all websockets connections to api server
-const wsproxy = httpProxy.createProxyServer();
-app.use((ctx) => {
-  ctx.respond = false;
-  wsproxy.web(ctx.req, ctx.res, { target: config.wsServerUrl });
-});
-
 const server = app.listen(process.env.HTTP || 3000); //eslint-disable-line no-process-env
-server.on('upgrade', (req, socket, head) => {
-  wsproxy.ws(req, socket, head, { target: config.wsServerUrl });
-});
-
 module.exports = app;
+
+httpProxy.createServer({
+  target: 'ws://localhost:8080',
+  ws: true
+}).listen(8014);
